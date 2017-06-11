ed58ac0c6c430ca5e37f147755dfd6aeacc0e464
Obscurant999 <alexey.kozyrev@gmail.com>
2017-06-11 09:06:35 +0300

    Version 1.1

--- src/MaxOf3_2.java
+++ src/MaxOf3_2.java
@@ -17,14 +17,15 @@ public class MaxOf3_2 {
         int z = in.nextInt();
 
         int[] Digitus = {x, y, z};
-        int max = 0;
+
+        int max = Digitus[0];
 
         for (int i = 0; i < Digitus.length; i++) {
-            if (Digitus[max] < Digitus[i]) {
-                max = i;
-                System.out.println("Наибольшее введенное число : " + Digitus[i]);
+            if (max < Digitus[i]) {
+                max = Digitus[i];
             }
         }
+        System.out.println("Наибольшее введенное число : " + max);
 
         //System.out.println(Digitus[0]);
         //System.out.println(Digitus[1]);
